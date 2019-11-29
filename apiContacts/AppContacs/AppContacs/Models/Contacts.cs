105984fb1d0e9a61146fc127f4cbaa6ae0a9a3e3
190799 <pablodanielmoreno13@gmail.com>
2019-11-29 16:42:03 -0400

    App ok

---
+++ apiContacts/AppContacs/AppContacs/Models/Contacts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppContacs.Models
+{
+    
+    public class Contacts
+    {
+        
+        public int ID { get; set; }
+        
+        public string Name { get; set; }
+        
+        public int Phone { get; set; }
+        
+        public string Bussines { get; set; }
+
+    }
+}
---
+++ apiContacts/AppContacs/AppContacs/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using AppContacs.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppContacs.ViewModels
+{
+    public class MainViewModel
+    {
+        #region properties
+        public List<Contacts> ListContacts { get; set; }
+        #endregion 
+
+        #region singleton
+        private static MainViewModel instance;
+        public static MainViewModel GetInstance()
+        {
+            if (instance==null)
+            {
+                instance = new MainViewModel();
+            }
+            return (instance);
+        }
+        #endregion
+
+        #region ViewModels
+        public ContactsViewModel contactsViewModel { get; set; }
+
+        #endregion 
+    }
+}
